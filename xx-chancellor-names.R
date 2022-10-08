402fabb226fae0cb8af786109f8612e1fbdf176b
bydata <ansgar.wolsing@gmail.com>
2022-10-08 16:27:46 +0200

    created a function which loads the geojson and produces the map; combined plot for 6 chancellors

--- xx-chancellor-names.R
+++ xx-chancellor-names.R
@@ -14,99 +14,11 @@ ggplot(de) +
 
 # https://wiki.openstreetmap.org/wiki/Map_features#Name
 
-#' Which keys to retrieve?
-#' https://help.openstreetmap.org/questions/64879/get-all-bicycle-infrastructure-for-a-city
-#' all roads that have additional tags indicating cycle infrastructure
-#'      (cycleway=lane and cycleway=track)
-#' all highway=cycleway
-#' all highway=footway and highway=path that allow bicycle use or are
-#'      intended for such (bicycle=yes, bicycle=designated, bicycle=official)
-
-
-
-
-## ------------------------------------------------
+#' HOW-TO
+#' All OSM data was extracted via Overpass Turbo API on https://overpass-turbo.eu/
+#' The queries can be found in the data folder
 
 library(geojsonio)
-
-chancellor_geoson_files <- here("data", "osm-chancellor-streets", c("erhard.geojson", "kohl.geojson"))
-
-erhard_geojson <-  geojson_read(chancellor_geoson_files[1])
-# geojson_list(erhard_geojson)
-
-erhard <- st_read(chancellor_geoson_files[1])
-
-erhard_birthplace <- getbb("Fürth, Bayern, Deutschland")
-erhard_birthplace_cnt <- c(
-  x = (erhard_birthplace["x", "min"] + erhard_birthplace["x", "max"]) / 2,
-  y = (erhard_birthplace["y", "min"] + erhard_birthplace["y", "max"]) / 2
-  )
-
-ggplot(de) +
-  geom_sf(color = "grey70", size = 0.3) + 
-  annotate(
-    "point",
-    x = erhard_birthplace_cnt["x"], y = erhard_birthplace_cnt["y"],
-    color = "grey4", shape = 21, fill = NA, stroke = 0.9, size = 3
-  ) +
-  annotate(
-    "text",
-    x = erhard_birthplace_cnt["x"], y = erhard_birthplace_cnt["y"],
-    color = "grey4", label = "Fürth",
-    hjust = -0.1, vjust = 2, family = "Oswald", size = 4
-  ) +
-  geom_sf(data = st_centroid(erhard), fill = "#59C3C3", size = 2.5,
-          shape = 21, color = "white") +
-  labs(
-    title = "Ludwig Erhard",
-    subtitle = "Chancellor 1963-1966"
-  ) + 
-  theme_void(base_family = "Helvetica Neue") +
-  theme(
-    plot.background = element_rect(color = "white", fill = "white"),
-    text = element_text(color = "grey20"),
-    plot.title = element_text(family = "Oswald", hjust = 0.5),
-    plot.subtitle = element_text(hjust = 0.5)
-  )
-ggsave(here("plots", "xx-chancellors-streets-erhard.png"), width = 6, height = 7)
-
-
-kohl <- st_read(chancellor_geoson_files[2])
-kohl_birthplace <- getbb("Ludwigshafen am Rhein, Deutschland")
-kohl_birthplace_cnt <- c(
-  x = (kohl_birthplace["x", "min"] + kohl_birthplace["x", "max"]) / 2,
-  y = (kohl_birthplace["y", "min"] + kohl_birthplace["y", "max"]) / 2
-)
-ggplot(de) +
-  geom_sf(color = "grey70", size = 0.3) + 
-  annotate(
-    "point",
-    x = kohl_birthplace_cnt["x"], y = kohl_birthplace_cnt["y"],
-    color = "grey4", shape = 21, fill = NA, stroke = 0.9
-  ) +
-  annotate(
-    "text",
-    x = kohl_birthplace_cnt["x"], y = kohl_birthplace_cnt["y"],
-    color = "grey4", label = "Ludwigshafen",
-    hjust = -0.1, vjust = -1, family = "Oswald", size = 4
-  ) +
-  geom_sf(data = st_centroid(kohl), fill = "#59C3C3", size = 2.5, 
-        shape = 21, color = "white") +
-  labs(
-    title = "Helmut Kohl",
-    subtitle = "Chancellor 1982-1998"
-  ) + 
-  theme_void(base_family = "Helvetica Neue") +
-  theme(
-    plot.background = element_rect(color = "white", fill = "white"),
-    text = element_text(color = "grey20"),
-    plot.title = element_text(family = "Oswald", hjust = 0.5, size = 20),
-    plot.subtitle = element_text(hjust = 0.5)
-  )
-ggsave(here("plots", "xx-chancellors-streets-kohl.png"), width = 6, height = 7)
-
-
-## Turn it into a function
 
 plot_chancellor_streets <- function(geojson_file, 
                                     birthplace_label = "",
@@ -126,6 +38,8 @@ plot_chancellor_streets <- function(geojson_file,
   
   ggplot(bgshape) +
     geom_sf(color = "grey70", size = 0.3) + 
+    geom_sf(data = st_centroid(streets_filtered), fill = "#59C3C3", size = 2.5,
+            shape = 21, color = "white") +
     annotate(
       "point",
       x = birthplace_cnt["x"], y = birthplace_cnt["y"],
@@ -137,8 +51,6 @@ plot_chancellor_streets <- function(geojson_file,
       color = "grey4", label = birthplace_label,
       hjust = -0.1, vjust = 2, family = "Oswald", size = 4
     ) +
-    geom_sf(data = st_centroid(streets_filtered), fill = "#59C3C3", size = 2.5,
-            shape = 21, color = "white") +
     labs(
       title = chancellor_name,
       subtitle = chancellor_period
@@ -147,20 +59,82 @@ plot_chancellor_streets <- function(geojson_file,
     theme(
       plot.background = element_rect(color = "white", fill = "white"),
       text = element_text(color = "grey20"),
-      plot.title = element_text(family = "Oswald", hjust = 0.5),
-      plot.subtitle = element_text(hjust = 0.5)
+      plot.title = element_text(family = "Oswald", color = "grey2", hjust = 0.5),
+      plot.subtitle = element_markdown(hjust = 0.5, lineheight = 1.25)
     )
 }
 
-plot_chancellor_streets(here("data", "osm-chancellor-streets", "erhard.geojson"),
-                        birthplace_label = "Fürth",
-                        birthplace_fullname = "Fürth, Deutschland",
-                        chancellor_name = "Ludwig Erhard",
-                        chancellor_period = "1963 to 1966")
 
-plot_chancellor_streets(here("data", "osm-chancellor-streets", "adenauer.geojson"),
+p1 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "adenauer.geojson"),
                         birthplace_label = "Köln",
                         birthplace_fullname = "Köln, Deutschland",
                         chancellor_name = "Konrad Adenauer",
-                        chancellor_period = "1949 to 1963")
+                        chancellor_period = "Chancellor from 1949 to 1963<br>*1876 \U2020 1967")
 ggsave(here("plots", "xx-chancellors-streets-adenauer.png"), width = 6, height = 7)
+
+
+p2 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "erhard.geojson"),
+                        birthplace_label = "Fürth",
+                        birthplace_fullname = "Fürth, Deutschland",
+                        chancellor_name = "Ludwig Erhard",
+                        chancellor_period = "Chancellor from 1963 to 1966<br>*1897 \U2020 1977")
+ggsave(here("plots", "xx-chancellors-streets-erhard.png"), width = 6, height = 7)
+
+
+p3 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "kiesinger.geojson"),
+                        birthplace_label = "Ebingen",
+                        birthplace_fullname = "Ebingen, Deutschland",
+                        chancellor_name = "Kurt Georg Kiesinger",
+                        chancellor_period = "Chancellor from 1966 to 1969<br>*1904 \U2020 1988")
+ggsave(here("plots", "xx-chancellors-streets-kiesinger.png"), width = 6, height = 7)
+
+
+p4 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "brandt.geojson"),
+                        birthplace_label = "Lübeck",
+                        birthplace_fullname = "Lübeck, Deutschland",
+                        chancellor_name = "Willy Brandt",
+                        chancellor_period = "Chancellor from 1969 to 1972<br>*1913 \U2020 1992")
+ggsave(here("plots", "xx-chancellors-streets-brandt.png"), width = 6, height = 7)
+
+
+p5 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "schmidt.geojson"),
+                        birthplace_label = "Hamburg",
+                        birthplace_fullname = "Hamburg, Deutschland",
+                        chancellor_name = "Helmut Schmidt",
+                        chancellor_period = "Chancellor from 1972 to 1982<br>*1918 \U2020 2015")
+ggsave(here("plots", "xx-chancellors-streets-schmidt.png"), width = 6, height = 7)
+
+
+p6 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "kohl.geojson"),
+                        birthplace_label = "Ludwigshafen",
+                        birthplace_fullname = "Ludwigshafen am Rhein, Deutschland",
+                        chancellor_name = "Helmut Kohl",
+                        chancellor_period = "Chancellor from 1982 to 1998<br>*1930 \U2020 2017")
+ggsave(here("plots", "xx-chancellors-streets-kohl.png"), width = 6, height = 7)
+
+
+p7 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "schroeder.geojson"),
+                        birthplace_label = "Mossenberg",
+                        birthplace_fullname = "Mossenberg, Deutschland",
+                        chancellor_name = "Gerhard Schröder",
+                        chancellor_period = "Chancellor from 1998 to 2005<br>*1994")
+ggsave(here("plots", "xx-chancellors-streets-schroeder.png"), width = 6, height = 7)
+
+
+library(patchwork)
+
+(p1 + p2 + p3) /
+  (p4 + p5 + p6) +
+  plot_annotation(
+    title = "Where the Streets Have German Chancellor's Names",
+    subtitle = "Streets which are named after chancellors of the Federal Republic of Germany",
+    caption = "Source: OpenStreetMap contributors. Visualisation: Ansgar Wolsing",
+    theme = theme(
+      text = element_text(family = "Helvetica Neue", color = "grey28"),
+      plot.title = element_text(hjust = 0.5, size = 24, color = "grey8",
+                                family = "Oswald"),
+      plot.subtitle = element_text(hjust = 0.5)
+    )
+  )
+  
+ggsave(here("plots", "xx-chancellors-streets-combined.png"), width = 10, height = 9)
