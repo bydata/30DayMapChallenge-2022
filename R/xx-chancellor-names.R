fa8f777cbf8e8b4e09b13f4e778a7b303e43f1f0
bydata <ansgar.wolsing@gmail.com>
2022-10-09 16:26:35 +0200

    moved R script to R folder

--- R/xx-chancellor-names.R
+++ R/xx-chancellor-names.R
@@ -69,7 +69,7 @@ p1 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "adenauer.g
                         birthplace_label = "Köln",
                         birthplace_fullname = "Köln, Deutschland",
                         chancellor_name = "Konrad Adenauer",
-                        chancellor_period = "Chancellor from 1949 to 1963<br>*1876 \U2020 1967")
+                        chancellor_period = "1949 to 1963<br>*1876 \U2020 1967")
 ggsave(here("plots", "xx-chancellors-streets-adenauer.png"), width = 6, height = 7)
 
 
@@ -77,7 +77,7 @@ p2 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "erhard.geo
                         birthplace_label = "Fürth",
                         birthplace_fullname = "Fürth, Deutschland",
                         chancellor_name = "Ludwig Erhard",
-                        chancellor_period = "Chancellor from 1963 to 1966<br>*1897 \U2020 1977")
+                        chancellor_period = "1963 to 1966<br>*1897 \U2020 1977")
 ggsave(here("plots", "xx-chancellors-streets-erhard.png"), width = 6, height = 7)
 
 
@@ -85,7 +85,7 @@ p3 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "kiesinger.
                         birthplace_label = "Ebingen",
                         birthplace_fullname = "Ebingen, Deutschland",
                         chancellor_name = "Kurt Georg Kiesinger",
-                        chancellor_period = "Chancellor from 1966 to 1969<br>*1904 \U2020 1988")
+                        chancellor_period = "1966 to 1969<br>*1904 \U2020 1988")
 ggsave(here("plots", "xx-chancellors-streets-kiesinger.png"), width = 6, height = 7)
 
 
@@ -93,7 +93,7 @@ p4 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "brandt.geo
                         birthplace_label = "Lübeck",
                         birthplace_fullname = "Lübeck, Deutschland",
                         chancellor_name = "Willy Brandt",
-                        chancellor_period = "Chancellor from 1969 to 1972<br>*1913 \U2020 1992")
+                        chancellor_period = "1969 to 1972<br>*1913 \U2020 1992")
 ggsave(here("plots", "xx-chancellors-streets-brandt.png"), width = 6, height = 7)
 
 
@@ -101,7 +101,7 @@ p5 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "schmidt.ge
                         birthplace_label = "Hamburg",
                         birthplace_fullname = "Hamburg, Deutschland",
                         chancellor_name = "Helmut Schmidt",
-                        chancellor_period = "Chancellor from 1972 to 1982<br>*1918 \U2020 2015")
+                        chancellor_period = "1972 to 1982<br>*1918 \U2020 2015")
 ggsave(here("plots", "xx-chancellors-streets-schmidt.png"), width = 6, height = 7)
 
 
@@ -109,7 +109,7 @@ p6 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "kohl.geojs
                         birthplace_label = "Ludwigshafen",
                         birthplace_fullname = "Ludwigshafen am Rhein, Deutschland",
                         chancellor_name = "Helmut Kohl",
-                        chancellor_period = "Chancellor from 1982 to 1998<br>*1930 \U2020 2017")
+                        chancellor_period = "1982 to 1998<br>*1930 \U2020 2017")
 ggsave(here("plots", "xx-chancellors-streets-kohl.png"), width = 6, height = 7)
 
 
@@ -117,7 +117,7 @@ p7 <- plot_chancellor_streets(here("data", "osm-chancellor-streets", "schroeder.
                         birthplace_label = "Mossenberg",
                         birthplace_fullname = "Mossenberg, Deutschland",
                         chancellor_name = "Gerhard Schröder",
-                        chancellor_period = "Chancellor from 1998 to 2005<br>*1994")
+                        chancellor_period = "1998 to 2005<br>*1994")
 ggsave(here("plots", "xx-chancellors-streets-schroeder.png"), width = 6, height = 7)
 
 
@@ -127,14 +127,16 @@ library(patchwork)
   (p4 + p5 + p6) +
   plot_annotation(
     title = "Where the Streets Have German Chancellor's Names",
-    subtitle = "Streets which are named after chancellors of the Federal Republic of Germany",
+    subtitle = "Places in Germany where streets are named after Chancellors of 
+    the Federal Republic of Germany. The chancellors' birthplaces are marked in each map.",
     caption = "Source: OpenStreetMap contributors. Visualisation: Ansgar Wolsing",
     theme = theme(
       text = element_text(family = "Helvetica Neue", color = "grey28"),
       plot.title = element_text(hjust = 0.5, size = 24, color = "grey8",
                                 family = "Oswald"),
-      plot.subtitle = element_text(hjust = 0.5)
+      plot.subtitle = element_textbox(
+        width = 0.8, hjust = 0.5, halign = 0.5, size = 14, lineheight = 1.25,
+        margin = margin(t = 4, b = 16))
     )
   )
-  
-ggsave(here("plots", "xx-chancellors-streets-combined.png"), width = 10, height = 9)
+ggsave(here("plots", "xx-chancellors-streets-combined.png"), width = 8, height = 9)
