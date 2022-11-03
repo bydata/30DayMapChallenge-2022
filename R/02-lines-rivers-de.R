62c6cc6875d063fd7889d0b81380929d4030f1e9
bydata <ansgar.wolsing@gmail.com>
2022-11-03 17:35:23 +0100

    day 2 - lines - Rivers of Germany

--- R/02-lines-rivers-de.R
+++ R/02-lines-rivers-de.R
@@ -5,12 +5,14 @@ library(here)
 
 de <- rnaturalearth::ne_countries(scale = 10, country = "Germany", returnclass = "sf")
 
-rivers <- opq(bbox = st_bbox(de), timeout = 1800) %>%
-  add_osm_feature(key = "waterway", value = "river") %>%
-  osmdata_sf()
-
-write_rds(rivers, here("data", "osm-rivers.rds"), compress = "gz")
-
+if (FALSE) {
+  rivers <- opq(bbox = st_bbox(de), timeout = 1800) %>%
+    add_osm_feature(key = "waterway", value = "river") %>%
+    osmdata_sf()
+  write_rds(rivers, here("data", "osm-rivers.rds"), compress = "gz")
+} else {
+  rivers <- read_rds(here("data", "osm-rivers.rds"))
+}
 
 rivers_multilines_prep <- rivers$osm_multilines %>% 
   mutate(
@@ -32,24 +34,21 @@ rivers_multilines_prep %>%
   head(10)
 
 
-p <- rivers_multilines_prep %>% 
+p1 <- rivers_multilines_prep %>% 
   ggplot(aes(color = de_length_km)) +
-  geom_sf(
-    data = de,
-    aes(geometry = geometry), fill = "grey16", color = "grey70", size = 0.01) +
   geom_sf(aes(size = de_length_km)) +
   geom_sf_label(
     data = slice_max(rivers_multilines_prep, order_by = de_length_km, n = 8),
     aes(label = name_consolidated),
     fill = alpha("grey22", 0.7), family = "Chivo", label.size = 0.1
     ) +
-  scico::scale_color_scico(palette = "nuuk") + #bamako #acton #nuuk
+  scico::scale_color_scico(palette = "nuuk") +
   scale_size_continuous(range = c(0.1, 0.8)) +
   guides(size = "none",
          color = "none") +
   labs(
     title = "Rivers of Germany",
-    subtitle = "River courses coloured by the length of the rivers within Germany",
+    subtitle = "River courses coloured by their length within Germany",
     caption = "Data: OpenStreetMap contributors. Visualisation: Ansgar Wolsing"
   ) + 
   theme_void(base_family = "Montserrat", base_size = 12) +
@@ -63,21 +62,37 @@ p <- rivers_multilines_prep %>%
     plot.caption = element_text(hjust = 0.5),
     plot.margin = margin(rep(4, 4))
   )
-ggsave(here("plots", "02-lines-rivers-de.png"), dpi = 600, width = 7, height = 8)
+ggsave(here("plots", "02-lines-rivers-de-without-shape.png"), dpi = 600, width = 7, height = 8)
 
-
-rivers$osm_lines %>% 
-  filter(name == "Oder") %>% 
-  select(osm_id, name, name.en, width, geometry) %>% 
-  mutate(length = st_length(geometry),
-         length_km = as.numeric(length) / 1000,
-         name_consolidated = ifelse(!is.na(name.en) & name.en != "", name.en, name)) %>% 
-  arrange(-length) %>% 
-  summarize(sum(length))
-
-
-rivers$osm_multilines %>% 
-  filter(name.en == "Oder") %>% 
-  ggplot() +
-  geom_sf(data = de) +
-  geom_sf(color = "red")
+p2 <- rivers_multilines_prep %>% 
+  ggplot(aes(color = de_length_km)) +
+  geom_sf(
+    data = de,
+    aes(geometry = geometry), fill = "grey16", color = "grey70", size = 0.01) +
+  geom_sf(aes(size = de_length_km)) +
+  geom_sf_label(
+    data = slice_max(rivers_multilines_prep, order_by = de_length_km, n = 8),
+    aes(label = name_consolidated),
+    fill = alpha("grey22", 0.7), family = "Chivo", label.size = 0.1
+  ) +
+  scico::scale_color_scico(palette = "nuuk") +
+  scale_size_continuous(range = c(0.1, 0.8)) +
+  guides(size = "none",
+         color = "none") +
+  labs(
+    title = "Rivers of Germany",
+    subtitle = "River courses coloured by their length within Germany",
+    caption = "Data: OpenStreetMap contributors. Visualisation: Ansgar Wolsing"
+  ) + 
+  theme_void(base_family = "Montserrat", base_size = 12) +
+  theme(
+    plot.background = element_rect(color = "grey8", fill = "grey8"),
+    panel.background = element_rect(color = NA, fill = NA),
+    text = element_text(color = "grey95"),
+    plot.title = element_text(color = "#FEFEFE", family = "Chivo", 
+                              size = 28, hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5),
+    plot.caption = element_text(hjust = 0.5),
+    plot.margin = margin(rep(4, 4))
+  ) 
+ggsave(here("plots", "02-lines-rivers-de-with-shape.png"), dpi = 600, width = 7, height = 8)
