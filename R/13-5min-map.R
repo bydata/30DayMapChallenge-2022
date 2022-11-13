86b07228b7c9c69b9c58e8bebfa54145de6b4683
bydata <ansgar.wolsing@gmail.com>
2022-11-13 09:49:22 +0100

    fill color + plot margin

--- R/13-5min-map.R
+++ R/13-5min-map.R
@@ -21,17 +21,19 @@ places_coords
 places_sf <- st_as_sf(places_coords, coords = c("x", "y"), crs = st_crs(de))
 
 ggplot() +
-  geom_sf(data = de) +
+  geom_sf(data = de, fill = "#E9EB87") +
   geom_sf(data = places_sf, col = "white", shape = 21, fill = "black") +
   ggrepel::geom_text_repel(
-    data = places_coords, aes(x, y, label = place), family = "Chivo") +
+    data = places_coords, aes(x, y, label = place), family = "Chivo",
+    point.padding = 3) +
   labs(
     title = "Places I've lived",
     caption = "Source: OpenStreetMap contributors"
   ) +
   theme_void(base_family = "Lato") +
   theme(plot.background = element_rect(color = "white", fill = "white"),
-        plot.title = element_text(hjust = 0.5, face = "bold")
+        plot.title = element_text(hjust = 0.5, face = "bold"),
+        plot.margin = margin(rep(2, 4))
         )
 
 ggsave(here::here("plots", "13-5min-map.png"), width = 4, height = 6)
